9caa72c5bd9aa3851f6a553b3a0df1d117742376
Priyam Soni <priyam05soni@gmail.com>
2025-02-16 17:17:33 +0530

    created a constant to have all the constant field data

---
+++ lib/app/constants/constants.dart
@@ -0,0 +1,15 @@
+class Constants {
+  static const String baseUrl = 'https://newsapi.org/v2';
+
+  static const logo = "assets/denik_logo.svg";
+
+  static const defaultImage =
+      "https://imgs.search.brave.com/DzOyhFO0fjZ-OnEOqCVIWnV6qHWY_T43bB-bfgHRUFk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA0Lzk5LzkzLzMx/LzM2MF9GXzQ5OTkz/MzExN19aQVVCZnYz/UDFIRU9zWkRybmti/TkN0NGpjM0FvZEFy/bC5qcGc";
+  static const String noInterntImage = 'assets/no_image.jpg';
+  static const String loadingImage = 'assets/three_dots.gif';
+
+  static const String cacheKey = 'news_cache';
+  static const String searchHistoryKey = 'recent_searches';
+
+  static const int pageSize = 5;
+}
